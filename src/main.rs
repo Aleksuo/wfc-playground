f047e614416096bc8c09474c34c3a97da4847b79
Aleksuo <aleksi.suoranta@gmail.com>
2026-03-04 23:57:43 +0200

    Add some test input bmp, transform input image to color mapped indices

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,52 @@
-use std::{
-    fs::{self, File},
-    io::Write,
-};
+use std::vec;
 
-use image::RgbImage;
+use image::{DynamicImage, ImageReader, Rgb};
 
-struct RGB {
-    red: u8,
-    green: u8,
-    blue: u8,
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let input_img = ImageReader::open("./input/test_input.bmp")?.decode()?;
+    overlap_model(input_img);
+    Ok(())
 }
 
-fn main() {
-    let width = 256;
-    let height = 256;
-    let max_val = 255;
-    let test_raster = {
-        let mut raster = Vec::new();
-        for _ in 0..width * height {
-            raster.push(RGB {
-                red: 255,
-                green: 0,
-                blue: 0,
-            });
+fn rgb8_to_string(pixel: Rgb<u8>) -> String {
+    format!("{},{},{}", pixel[0], pixel[1], pixel[2])
+}
+
+fn sample_dynamic_image(img: &DynamicImage) -> (u32, u32, Vec<u16>, Vec<String>) {
+    let img = img.to_rgb8();
+    let (width, height) = img.dimensions();
+    let mut sample: Vec<u16> = vec![0; (height * width) as usize];
+    let mut colors: Vec<String> = vec![];
+    for (x, y, pixel) in img.enumerate_pixels() {
+        let mut k = 0;
+        let pixel_string = rgb8_to_string(*pixel);
+        for i in 0..colors.len() {
+            k = i;
+            if colors[i] == pixel_string {
+                break;
+            }
         }
-        raster
-    };
-    write_bmp("test", width, height, &test_raster);
+        if k + 1 >= colors.len() {
+            colors.push(pixel_string);
+        }
+        let index = x + y * width;
+        sample[index as usize] = k as u16;
+    }
+    (width, height, sample, colors)
 }
 
-fn write_bmp(name: &str, width: u32, height: u32, raster: &[RGB]) {
-    let bytes: Vec<u8> = raster
-        .iter()
-        .flat_map(|pixel| [pixel.red, pixel.green, pixel.blue])
-        .collect();
-    let img = RgbImage::from_raw(width, height, bytes).unwrap();
-    fs::create_dir_all(".output").unwrap();
-    let path = format!(".output/{}.bmp", name);
-    img.save(path).unwrap();
+fn overlap_model(img: DynamicImage) {
+    let (width, height, sample, _color_map) = sample_dynamic_image(&img);
+    print_sampled_input(width, height, &sample);
+}
+
+fn print_sampled_input(width: u32, height: u32, sample_arr: &Vec<u16>) {
+    println!("Sampled input:");
+    for i in 0..height {
+        for j in 0..width {
+            let index = j + i * height;
+            print!("{} ", sample_arr[index as usize]);
+        }
+        println!();
+    }
 }
