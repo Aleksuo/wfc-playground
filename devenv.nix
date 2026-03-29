17707c2d4a7e66670ed197842af9400649527326
Aleksuo <aleksi.suoranta@gmail.com>
2026-03-29 15:47:23 +0300

    WFC-2: Add showOutput to other tasks too

--- devenv.nix
+++ devenv.nix
@@ -11,11 +11,13 @@
   tasks."dev:fmt:check" = {
     exec = "cargo fmt --check";
     description = "Check Rust formatting";
+    showOutput = true;
   };
 
   tasks."dev:fmt:write" = {
     exec = "cargo fmt";
     description = "Format all Rust files";
+    showOutput = true;
   };
 
   tasks."dev:test" = {
