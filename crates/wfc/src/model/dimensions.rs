383bd6e9e518aeb2ec15f03fae8a64ca29328827
Aleksi <aleksi.suoranta@gmail.com>
2026-08-01 13:11:31 +0300

    WFC-36: Dimensions struct for generic representation of handled dimensions

---
+++ crates/wfc/src/model/dimensions.rs
@@ -0,0 +1,44 @@
+#[derive(Copy, Clone)]
+pub struct Dimensions<const N: usize>([u32; N]);
+
+impl<const N: usize> Dimensions<N> {
+    pub fn valid(&self) -> Result<(), DimensionValidationError> {
+        for i in self.0 {
+            if i == 0 {
+                return Err(DimensionValidationError::EmptyDimension);
+            }
+        }
+        Ok(())
+    }
+
+    pub fn total(&self) -> u32 {
+        self.0.into_iter().reduce(|a, b| a * b).unwrap_or_default()
+    }
+
+    pub fn get(&self, index: usize) -> u32 {
+        self.0[index]
+    }
+
+    pub fn new(dims: [u32; N]) -> Self {
+        Self(dims)
+    }
+}
+
+pub enum DimensionValidationError {
+    EmptyDimension,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    mod total {
+        use super::*;
+
+        #[test]
+        fn multiplies_every_axis() {
+            assert_eq!(Dimensions::new([4, 3]).total(), 12);
+            assert_eq!(Dimensions::new([4, 3, 2]).total(), 24);
+        }
+    }
+}
