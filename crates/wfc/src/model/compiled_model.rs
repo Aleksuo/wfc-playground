7b7c04f0479bf2efa339a3d08dd13c8e7dc67033
Aleksi Suoranta <aleksi.suoranta@gmail.com>
2026-08-09 22:24:50 +0300

    WFC-39: Make the CompiledModel fields private, add getters and a constructor method (#27)

--- crates/wfc/src/model/compiled_model.rs
+++ crates/wfc/src/model/compiled_model.rs
@@ -2,10 +2,42 @@ use crate::model::rule_model::{AdjadencyRules, FrequencyHints};
 
 #[derive(Debug)]
 pub struct CompiledModel {
-    pub adj_rules: AdjadencyRules,
-    pub frequency_hints: FrequencyHints,
-    pub num_patterns: usize,
-    pub num_directions: usize,
+    adj_rules: AdjadencyRules,
+    frequency_hints: FrequencyHints,
+    num_patterns: usize,
+    num_directions: usize,
+}
+
+impl CompiledModel {
+    pub(crate) fn new(
+        adj_rules: AdjadencyRules,
+        frequency_hints: FrequencyHints,
+        num_directions: usize,
+    ) -> Self {
+        let num_patterns = frequency_hints.weights.len();
+        Self {
+            adj_rules,
+            frequency_hints,
+            num_patterns,
+            num_directions,
+        }
+    }
+
+    pub(crate) fn adj_rules(&self) -> &AdjadencyRules {
+        &self.adj_rules
+    }
+
+    pub(crate) fn frequency_hints(&self) -> &FrequencyHints {
+        &self.frequency_hints
+    }
+
+    pub(crate) fn num_patterns(&self) -> usize {
+        self.num_patterns
+    }
+
+    pub(crate) fn num_directions(&self) -> usize {
+        self.num_directions
+    }
 }
 
 #[cfg(test)]
