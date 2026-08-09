6793b58a7316c28a4caa7082902ff0488855c645
Aleksi <aleksi.suoranta@gmail.com>
2026-08-09 12:48:55 +0300

    WFC-36: Add error messages for solver errors

--- crates/wfc/src/model/solver_run_configuration.rs
+++ crates/wfc/src/model/solver_run_configuration.rs
@@ -1,4 +1,7 @@
-use std::num::NonZeroU32;
+use std::{
+    fmt::{self},
+    num::NonZeroU32,
+};
 
 use crate::Dimensions;
 
@@ -7,13 +10,30 @@ pub enum ContradictionStrategy {
     Retry { max_attempts: NonZeroU32 },
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub enum SolverError {
     AttemptsExhausted,
 }
 
+impl fmt::Display for SolverError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            SolverError::AttemptsExhausted => write!(f, "Maximum number of retries was reached."),
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub(crate) enum SolverRunError {
     Contradiction,
+}
+
+impl fmt::Display for SolverRunError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            SolverRunError::Contradiction => write!(f, "Solver run ran into a contradiction!"),
+        }
+    }
 }
 
 pub struct SolverRunConfiguration {
